b6017373ba9c923c190b321fa8d02e69af50fd45
PC104-01 <PC104-01@eklya.fr>
2021-11-08 18:01:11 +0100

    Added components to structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
+import Footer from './components/Footer.js';
+import Header from './components/Header';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" width="50" />
-        <h2>Header</h2>
-      </header>
+     <Header />
+     
       <section>
         <h1>Contenu</h1>
-        <button type="button" className="btn btn-primary">Primary</button>
+
       </section>
-      <footer>
-        <h2>Footer</h2>
-      </footer>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,8 @@
+const Footer = () => {
+   return(
+    <footer className="bg-dark text-white text-center p-2">
+    <p>&copy; CCI DWWM</p>
+   </footer>
+   ) 
+}
+export default Footer
