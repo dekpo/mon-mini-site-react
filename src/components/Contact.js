51234247173289aeaf4d53fee5451740781a7579
Dekpo-Wyna Yologaza <dw.yologaza@yahoo.fr>
2021-11-10 14:21:00 +0100

    Made a build

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -5,9 +5,12 @@ const Contact = () => {
           <div className="row"><h1>Contact</h1></div>
           <div className="row">
             <div className="col-12 col-md-6">
-              <img className="img-fluid" src="https://cdn.pixabay.com/photo/2020/04/14/15/35/beach-5043177_960_720.jpg" alt="Image plage" />
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2783.04087965204!2d4.80314121556775!3d45.770370979105756!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47f4eb7391fcf843%3A0xf4170493513eaa9f!2sCCI%20Formation%20LYON%20M%C3%89TROPOLE%20Saint-Etienne%20Roanne!5e0!3m2!1sen!2sfr!4v1636546708635!5m2!1sen!2sfr" width="100%" height="450" loading="lazy"></iframe>
             </div>
-            <div className="col-12 col-md-6">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
+            <div className="col-12 col-md-6">
+              <p>Nous sommes ici !</p>
+              <p>Venez nous offrir un café avant 11 heure ! </p>
+              </div>
           </div>
         </div>
       </section>
