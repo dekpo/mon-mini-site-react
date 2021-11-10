66dbe4073f66fdde32428bfd0def78f0baf9a301
Dekpo-Wyna Yologaza <dw.yologaza@yahoo.fr>
2021-11-10 14:27:08 +0100

    Made a new build

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,15 +5,15 @@ const Header = () => {
         <header className="App-header">
         <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <div class="container-fluid">
-            <a class="navbar-brand" href="/"> <img src={logo} className="App-logo" alt="logo" width="50" /></a>
+            <a class="navbar-brand" href="/mon-mini-site-react/"> <img src={logo} className="App-logo" alt="logo" width="50" /></a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="/">Home</a>
-                <a class="nav-link" href="/gallery">Gallery</a>
-                <a class="nav-link" href="/contact">Contact</a>
+                <a class="nav-link active" aria-current="page" href="/mon-mini-site-react/">Home</a>
+                <a class="nav-link" href="/mon-mini-site-react/gallery">Gallery</a>
+                <a class="nav-link" href="/mon-mini-site-react/contact">Contact</a>
               </div>
             </div>
           </div>
