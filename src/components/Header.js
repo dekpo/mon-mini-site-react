94ba683fc31fcc9c133f0aeed965261c8c10f598
Dekpo-Wyna Yologaza <dw.yologaza@yahoo.fr>
2021-11-18 12:29:30 +0100

    Made a new build in the docs folder

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,21 @@
 import logo from '../assets/logo.svg';
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
     return(
         <header className="App-header">
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/"> <img src={logo} className="App-logo" alt="logo" width="50" /></a>
+            <NavLink className="navbar-brand" to="/"> <img src={logo} className="App-logo" alt="logo" width="50" /></NavLink>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div className="navbar-nav">
                
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
-                <a className="nav-link" href="/gallery">Gallery</a>
-                <a className="nav-link" href="/contact">Contact</a>
+                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
+                <NavLink className="nav-link" to="/gallery">Gallery</NavLink>
+                <NavLink className="nav-link" to="/contact">Contact</NavLink>
             
               </div>
             </div>
